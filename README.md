cb09511c192834ad40a44657f491743c106c95e9
Qwame Palmer <qwamepalmer@gmail.com>
2024-12-19 19:55:40 -0500

    Made a function using if statments instead of an array to get random choices from the computer between the three choices of the game

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Rock-Paper-Scissors
+
+Rock Paper Scissors Project for the Odin Project
---
+++ script.js
@@ -0,0 +1,22 @@
+
+console.log("hello world");
+
+
+
+
+
+
+function getComputerChoice(){
+    const randomNum = 
+    Math.floor( Math.random() * 3)
+    if (randomNum === 0) {
+        return "Rock";
+    } else if (randomNum === 1) {
+        return "Paper";
+    }else {
+        return "Scissors";
+    }
+   
+} 
+
+console.log(getComputerChoice)
