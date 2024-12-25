3ba3c52ba2a816473dcb134581aef1d31dd2f88b
Qwame Palmer <qwamepalmer@gmail.com>
2024-12-24 21:28:01 -0500

    made a function for a round, hopefully it works. Wanted to use a more efficient way of writing the code block but this works for the stage I'mat currently, I understand the course's intentions.

--- script.js
+++ script.js
@@ -33,3 +33,35 @@ function getHumanChoice(){
 } 
 
 console.log(getHumanChoice)
+
+let humanScore = 0
+let computerScore = 0
+
+function playRound(getHumanChoice, getComputerChoice){
+    humanChoice.toLowerCase()
+    if (humanChoice ===  Rock && computerScore === Rock ){
+        return "It's a tie"
+    }else if (humanChoice ===  Paper && computerScore === Paper ){
+            return "It's a tie"
+    }else if (humanChoice ===  Scissors && computerScore === Scissors ){
+                return "It's a tie"
+    }else if (humanChoice ===  Rock && computerScore === Scissors ){
+        return "You Win, Mortal." + humanScore++
+    }else if (humanChoice ===  Rock && computerScore === Paper ){
+        return "Death awaits you." + computerScore++
+    }else if (humanChoice ===  Paper && computerScore === Scissors ){
+        return "Death awaits you." + humanScore++
+    }else if (humanChoice ===  Paper && computerScore === Rock ){
+        return "You Win, Mortal." + humanScore++
+    }else if (humanChoice ===  Scissors && computerScore === Rock ){
+        return "Death awaits you." + humanScore++
+    }else if (humanChoice ===  Scissors && computerScore === Scissors ){
+        return "You Win, Mortal." + humanScore++
+}
+}
+
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
