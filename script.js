03b80107cee05a6b7df06370181ca77834d1d70d
Qwame Palmer <qwamepalmer@gmail.com>
2025-01-01 01:54:25 -0500

    Made a hell of amount of adjustments and this took mealot longer than expected, taught me how to think logically

--- script.js
+++ script.js
@@ -1,73 +1,74 @@
 
 
 function getComputerChoice(){
-    const randomNum = 
-    Math.floor( Math.random() * 3)
-    if (randomNum === 0) {
-        return "Rock";
-    } else if (randomNum === 1) {
-        return "Paper";
+    let computerChoice = 
+    Math.floor( Math.random() * 3 + 1);
+    if (computerChoice === 1) {
+        return computerChoice = "rock";
+    } else if (computerChoice === 2) {
+        return computerChoice = "paper";
     }else {
-        return "Scissors";
+        return computerChoice ="scissors";
     }
    
 } 
 
-console.log(getComputerChoice)
+
+
 
 function getHumanChoice(){
-    console.log("Choice?")
-    const choice = prompt();
-    if (choice === "Rock" ){
-        return `You chose ${choice}`;
-    }else if (choice === "Paper"){
-        return `You chose ${choice}`;
-    }else if (choice === "Scissors")
-        return `You chose ${choice}`;
-        else {
-            return "invalid"; 
-        }
+    while (true){
+    let humanChoice = prompt('Choose your tool...Rock,Paper or Scissors?');
+    humanChoice = humanChoice.toLowerCase();
+    if (humanChoice == "rock" || humanChoice == "paper" || humanChoice == "scissors"){
+    return humanChoice;}
+    else{
+        continue;
+    }
+}
+}
 
-    
+
+console.log('Welcome to the Game')
    
-} 
-
-console.log(getHumanChoice)
-
-
-
-
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-playRound(humanSelection, computerSelection);
-
-function playGame(){
-
 let humanScore = 0
 let computerScore = 0
 
-function playRound(humanChoice, computerChoice){
-    humanChoice.toLowerCase()
-    if (humanChoice ===  Rock && computerScore === Rock ){
-        return "It's a tie"
-    }else if (humanChoice ===  Paper && computerScore === Paper ){
-            return "It's a tie"
-    }else if (humanChoice ===  Scissors && computerScore === Scissors ){
-                return "It's a tie"
-    }else if (humanChoice ===  Rock && computerScore === Scissors ){
-        return "You Win, Mortal." + humanScore++
-    }else if (humanChoice ===  Rock && computerScore === Paper ){
-        return "Death awaits you." + computerScore++
-    }else if (humanChoice ===  Paper && computerScore === Scissors ){
-        return "Death awaits you." + computerScore++
-    }else if (humanChoice ===  Paper && computerScore === Rock ){
-        return "You Win, Mortal." + humanScore++
-    }else if (humanChoice ===  Scissors && computerScore === Rock ){
-        return "Death awaits you." + computerScore++
-    }else if (humanChoice ===  Scissors && computerScore === Scissors ){
-        return "You Win, Mortal." + humanScore++
+
+
+
+function playRound(humanChoice, computerChoice) {
+    if(humanSelection === "rock" && computerSelection === "scissors" || 
+        humanSelection === "paper" && computerSelection === "rock" ||
+        humanSelection === "scissors" && computerSelection === "paper" ) { 
+           humanScore++; 
+           console.log( 'you chose:' + humanSelection + ' '+ 'computer chose:' + computerSelection) 
+           console.log(`You win, ${humanSelection} beats ${computerSelection}`)
+        }
+        else if (humanChoice === computerChoice) {
+            console.log('Try again'); 
+        } 
+   else {  
+     computerScore++;  
+     console.log( 'you chose:' + humanSelection + ' '+ 'computer chose:' + computerSelection) 
+     console.log(`You lose, ${computerSelection} beats ${humanSelection}`)
+   } 
+ console.log( `you: ${humanScore} - computer: ${computerScore}`)  
+  }
+  
+  
+  const humanSelection = getHumanChoice();
+  const computerSelection = getComputerChoice();
+
+  function playGame(){
+  for (let i = 0; i < 5; i++) {
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection);
 }
-}
-    
-}
+  }
+
+
+  
+  playGame();
+
